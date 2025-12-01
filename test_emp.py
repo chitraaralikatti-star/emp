273384ea24527572c04d285a48efdbf62a2c880f
chitraaralikatti <chitraaralikatti@gmail.com>
2025-12-01 16:00:40 +0530

    python

--- test_emp.py
+++ test_emp.py
@@ -1,9 +1,9 @@
-from employee import employee_details
+from emp import employee_details
 def test_employee_details():
     expected_output = (
-        "Employee Name:Alice\n"
+        "Employee Name:chitra\n"
         "Employee ID:E1001\n"
         "Department:IT\n"
         "Salary:55000\n"
     )
-    assert employee_details("Alice", "E1001", "IT", 55000) == expected_output
+    assert employee_details("chitra", "E1001", "IT", 55000) == expected_output
